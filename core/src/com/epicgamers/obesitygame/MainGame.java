3bb1ecf189e90abfa3c8d1e1256da80ce7634518
Junha Lee <junhalee02@gmail.com>
2019-12-23 18:16:31 -0800

    Title screen

--- core/src/com/epicgamers/obesitygame/MainGame.java
+++ core/src/com/epicgamers/obesitygame/MainGame.java
@@ -3,18 +3,37 @@ package com.epicgamers.obesitygame;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MainGame extends ApplicationAdapter {
 	
+	private Texture title;
+	private OrthographicCamera camera;
+	private SpriteBatch batch;
+	
 	@Override
 	public void create () {
+		title = new Texture(Gdx.files.internal("title.png"));
+		//title.setScale(3);
 		
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 1280, 720);
+		
+		batch = new SpriteBatch();
 	}
 
 	@Override
 	public void render () {
+		
+		Gdx.gl.glClearColor(210/255f, 248/255f, 252/255f, 1);
+	    Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		batch.draw(title, 0, 0, 1280, 720);
+		batch.end();
 		
 	}
 	
