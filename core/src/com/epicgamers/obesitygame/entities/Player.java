4f1f4fbe113c85b53797815f14d5b795a36087f0
Junha Lee <junhalee02@gmail.com>
2019-12-25 20:53:07 -0800

    Spritework I guess

--- core/src/com/epicgamers/obesitygame/entities/Player.java
+++ core/src/com/epicgamers/obesitygame/entities/Player.java
@@ -1,6 +1,11 @@
 package com.epicgamers.obesitygame.entities;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
 public class Player extends Entity {
+	
+	Animation<TextureRegion> walkCycle;
 	
 	public Player(float x, float y, float width, float height, String src) {
 		super(x, y, width, height, src);
