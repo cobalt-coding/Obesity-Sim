3c8aaf29a48506e12a64fbcde46e77b95b4a42ae
Jared Swislow <jayswis2002@gmail.com>
2019-12-27 15:51:52 -0800

    pause screen is food
    food xd

--- core/src/com/epicgamers/obesitygame/scenes/PauseScreen.java
+++ core/src/com/epicgamers/obesitygame/scenes/PauseScreen.java
@@ -5,16 +5,20 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
-
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.epicgamers.obesitygame.scenes.SceneEnum.Scene;
 
 public class PauseScreen {
 	//make texture variables
 	Texture pause;
+	Button playButton = new Button();
+	Skin playButtonSkin = new Skin();
 	
 		public PauseScreen() {
 			//initialize texture
-			pause = new Texture(Gdx.files.internal("badlogic.jpg"));
+			pause = new Texture(Gdx.files.internal("food.png"));
+			
 		}
 		
 		public Scene render(Batch batch) {
@@ -22,6 +26,11 @@ public class PauseScreen {
 			//Regular use for rendering (no need to worry about batch.begin() or batch.end)
 			Gdx.gl.glClearColor(210/255f, 248/255f, 252/255f, 1);
 			Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+			
+			/*
+			if(playButton.isPressed()) {
+				System.out.println("button pressed");
+			}*/
 			
 			batch.draw(pause, 0, 0, 1280, 720);
 			
