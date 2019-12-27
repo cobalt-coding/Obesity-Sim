92da46a81988d656e10541681f34daa54bbcc10c
Junha Lee <junhalee02@gmail.com>
2019-12-26 22:20:46 -0800

    Array of Edibles

--- core/src/com/epicgamers/obesitygame/scenes/Game.java
+++ core/src/com/epicgamers/obesitygame/scenes/Game.java
@@ -1,10 +1,13 @@
 package com.epicgamers.obesitygame.scenes;
 
+import java.util.Iterator;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.utils.Array;
 import com.epicgamers.obesitygame.entities.Edible;
 import com.epicgamers.obesitygame.entities.Player;
 import com.epicgamers.obesitygame.scenes.SceneEnum.Scene;
@@ -14,11 +17,17 @@ public class Game {
 	//make texture variables
 	Player player;
 	Edible foodTest;
+	private Array<Edible> food;
 	
 	public Game() {
 		//initialize texture
 		player = new Player(100, 100);
 		foodTest = new Edible(20, 20, 20, 20, "food.png");
+		food = new Array<Edible>();
+		
+		for (int i = 0 ; i < 5 ; i++) {
+			food.add(new Edible((float)Math.random()*1280, (float)Math.random()*720, 20, 20, "food.png"));
+		}
 	}
 	
 	public Scene render(Batch batch) {
@@ -29,6 +38,10 @@ public class Game {
 		
 		player.render(batch);
 		foodTest.render(batch);
+		
+		for (Edible edible : food) {
+			edible.render(batch);
+		}
 		
 		//Conditional returning Scene to change
 		return Scene.GAME;
